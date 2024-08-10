34662f57a6b4f6839c810d3d7c9d88e456f1d0a1
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-08-10 20:42:44 +0530

    Fixed Card Layout

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,36 @@
+import CardLayout from "../common/CardLayout";
+
 const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <CardLayout>
+      <div className="flex items-center">
+        <img
+          src="https://avatars.githubusercontent.com/u/1?v=4"
+          alt="avatar"
+          className="rounded-full w-24 h-24"
+        />
+        <div className="flex flex-col w-full px-4">
+          <d className="flex justify-between items-center w-full">
+            <h1 className="text-2xl font-bold">mojombo</h1>
+            <span className="text-sm text-gray-300">Joined on 2007-10-20</span>
+          </d>
+          <p className="text-md text-gray-300">@mojombo</p>
+          <p className="text-sm text-gray-300 my-4">This profile has no bio</p>
+          <div className="flex justify-around items-center bg-custom-primary p-4 rounded-lg">
+            <p className="text-sm text-gray-300 flex flex-col justify-center items-center">
+              Repos <span className="text-lg">61</span>
+            </p>
+            <p className="text-sm text-gray-300 flex flex-col justify-center items-center">
+              Followers <span className="text-lg">61</span>
+            </p>
+            <p className="text-sm text-gray-300 flex flex-col justify-center items-center">
+              Following <span className="text-lg">61</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </CardLayout>
+  );
+};
 
-export default Card
+export default Card;
